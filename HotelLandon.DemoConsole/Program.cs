998e9d35fa9dee4854380bb59d99cfc6d43e2d56
Andres Talavera <andres.talavera@live.fr>
2022-01-17 11:24:20 +0100

    Récupérer des informations depuis la console

---
+++ HotelLandon.DemoConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HotelLandon.DemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prénom");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Nom de famille");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Date de naissance");
+            string birthDate = Console.ReadLine();
+        }
+    }
+}
