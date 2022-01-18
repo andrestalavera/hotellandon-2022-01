8d4aeae7b92bd636fc15d33fcf431e4f215d96f0
Andres Talavera <andres.talavera@live.fr>
2022-01-18 15:30:24 +0100

    DemoEfCore + Solution Visual Studio

--- HotelLandon.Data/HotelLandonContext.cs
+++ HotelLandon.Data/HotelLandonContext.cs
@@ -13,5 +13,20 @@ namespace HotelLandon.Data
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HotelLandon;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Console.WriteLine("OnModelCreating");
+            modelBuilder.Entity<Room>().HasData(
+                new Room { Id = 1, Number = 1, Floor = 0 },
+                new Room { Id = 2, Number = 2, Floor = 0 },
+                new Room { Id = 3, Number = 3, Floor = 0 },
+                new Room { Id = 4, Number = 4, Floor = 0 },
+                new Room { Id = 5, Number = 5, Floor = 0 },
+                new Room { Id = 6, Number = 6, Floor = 0 },
+                new Room { Id = 7, Number = 7, Floor = 0 },
+                new Room { Id = 8, Number = 8, Floor = 0 }
+            );
+        }
     }
 }
---
+++ HotelLandon.DemoEfCore/Program.cs
@@ -0,0 +1,39 @@
+﻿using HotelLandon.Data;
+using HotelLandon.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HotelLandon.DemoEfCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Saisir les informations d'un client NOM PRENOM DATE jj/mm/aaaa");
+            string input = Console.ReadLine();
+
+            string[] split = input.Split(' ');
+            string lastName = split[0];
+            string firstName = split[1];
+            DateTime birthDate = DateTime.Parse(split[2]);
+            
+            Customer customer = new Customer()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                BirthDate = birthDate
+            };
+
+            using (HotelLandonContext context = new HotelLandonContext())
+            {
+                context.Database.EnsureCreated();
+                context.Database.Migrate();
+                // on peut ajouter tout type d'objet présent dans le DbContext
+                context.Add(customer);
+                // on ne peut ajouter que des objets de type Customer :
+                // context.Customers.Add(customer);
+                context.SaveChanges();
+            }            
+        }
+    }
+}
