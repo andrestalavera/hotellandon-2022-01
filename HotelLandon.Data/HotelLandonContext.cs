f08c82b6e987a5b6ac76770f833ab70b81fad97b
Andres Talavera <andres.talavera@live.fr>
2022-01-18 11:40:44 +0100

    HotelLandonContext

---
+++ HotelLandon.Data/HotelLandonContext.cs
@@ -0,0 +1,17 @@
+﻿using HotelLandon.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HotelLandon.Data
+{
+    public class HotelLandonContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HotelLandon;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
